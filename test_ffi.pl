06b48bae5db28f73620c82973221929b4ec6f9cf
Peter Ludemann <peter.ludemann@gmail.com>
2023-02-08 22:30:38 -0800

    TEST: PL_open_query with various options plus some foreign.doc examples

--- test_ffi.pl
+++ test_ffi.pl
@@ -52,7 +52,9 @@
 
 test_ffi :-
     run_tests([ ffi,
-                wchar
+                wchar,
+                scan,
+                call
 	      ]).
 
 :- begin_tests(ffi).
@@ -102,6 +104,17 @@ test(range_ffialloc6a, error(type_error(integer,a))) :-
     range_ffialloc(a, 10, _).
 test(range_ffialloc6b, error(type_error(integer,foo))) :-
     range_ffialloc(1, foo, _).
+
+test(make_list, Env == ['SHELL=/bin/bash', 'TERMCAP=', 'PWD=/home/programmer/src/swipl-devel', 'LANG=en_US.UTF-8']) :-
+    ffi_get_environ1(Env).
+test(make_list, Env == ['SHELL=/bin/bash', 'TERMCAP=', 'PWD=/home/programmer/src/swipl-devel', 'LANG=en_US.UTF-8']) :-
+    ffi_get_environ2(Env).
+test(make_list, Env == ['SHELL=/bin/bash', 'TERMCAP=', 'PWD=/home/programmer/src/swipl-devel', 'LANG=en_US.UTF-8']) :-
+    ffi_get_environ3(Env).
+
+test(get_list, Output == "fred\ncharles\nmindy\n") :-
+    with_output_to(string(Output),
+                   ffi_write_atoms(current_output, [fred,charles,mindy])).
 
 :- end_tests(ffi).
 
@@ -159,6 +172,17 @@ test(char_1, all(Result == ["//", "/ /", "/abC/", "/Hello World!/"])) :-
     ;   atom_ffi('Hello World!',   Result)
     ).
 
+w_atom_ffi(Atom, String) :-
+    with_output_to(string(String), w_atom_ffi_(current_output, Atom)).
+
+atom_ffi(Atom, String) :-
+    with_output_to(string(String), atom_ffi_(current_output, Atom)).
+
+:- end_tests(wchar).
+
+
+:- begin_tests(scan).
+
 test(scan_options, [Options == options(true, 5, foo(bar), [], "")]) :-
     ffi_options(Options, [quoted(true), length(5), callback(foo(bar))]).
 test(scan_options, [Options == options(true, 5, foo(bar), qqsv, "DESCR")]) :-
@@ -178,34 +202,120 @@ test(scan_options, [error(type_error(option,123))]) :- % TODO: is this intended 
 test(scan_options, [error(type_error(option,123))]) :- % TODO: is this intended behavior?
     ffi_options(_Options, [token(qqsv), 123, descr("DESCR"), length(5), callback(foo(bar))]).
 
+:- end_tests(scan).
+
+
+:- begin_tests(call).
+
+% New code should use PL_Q_PASS_EXCEPTION or PL_Q_CATCH_EXCEPTION, so
+% there are only minimal tests for PL_Q_NORMAL or flags=0.
+
+% In the following tests, the `FlagsStr` is specified, as a
+% double-check that query_flags/2 has generated the correct bits in
+% the flag.
+
+test(ffi_call, X == a) :-
+    ffi_call(X = a, [normal], "normal").
+test(ffi_call, X == a) :-
+    ffi_call(X = a, [pass_exception], "pass_exception").
+test(ffi_call, X == a) :-
+    ffi_call(X = a, [catch_exception], "catch_exception").
+
+test(ffi_call) :-
+    ffi_call(unknown_pred(foo), [nodebug,pass_exception,clear_return_true],
+             "nodebug,pass_exception,clear_return_true",
+             Exc_0, Exc_qid, Exc_0_2, NextRc),
+    assertion(NextRc == 0),
+    assertion(Exc_0 == Exc_0_2),
+    match_existence_error(Exc_0, MatchExc_0),
+    match_existence_error(Exc_qid, MatchExc_qid),
+    % The terms from Exc_0 and Exc_qid are different
+    assertion(MatchExc_0.1 \== MatchExc_qid.1).
+test(ffi_call) :-
+    ffi_call(unknown_pred(foo), [nodebug,catch_exception,clear_return_true],
+             "nodebug,catch_exception,clear_return_true",
+             Exc_0, Exc_qid, Exc_0_2, NextRc),
+    assertion(NextRc == 0),
+    assertion(Exc_0 == "<null-term>"),
+    assertion(Exc_0_2 == "<null-term>"),
+    match_existence_error(Exc_qid, _MatchExc_qid).
+
+match_existence_error(Str, Matches) :-
+    % Match 1: the term_t value
+    % Match 2: the contents of context(...)
+    MatchRE = "^<([0-9]+)>:error\\(existence_error\\(procedure,test_ffi:unknown_pred/1\\),context\\((.*)\\)\\)$",
+    (   re_matchsub(MatchRE, Str, Matches)
+    *-> true
+    ;   assertion(re_matchsub(MatchRE, Str, Matches))
+    ).
+
+test(ffi_call_no_options, blocked('Activates trace/debug mode')) :-
+    ffi_call(non_existant_pred(foo), [], "").
+test(ffi_call_normal, blocked('Invokes debugger')) :-
+    ffi_call(non_existant_pred(foo), [normal], "normal").
+test(ffi_call_normal, blocked('Invokes debugger')) :-
+    catch(ffi_call(non_existant_pred(foo), [normal], "normal"), _E, true).
+
 ffi_call(Goal, Flags) :-
     query_flags(Flags, CombinedFlag),
-    ffi_call_(Goal, CombinedFlag).
+    ffi_call_exc(Goal, CombinedFlag, _, _, _, _).
 
-:- end_tests(wchar).
+ffi_call(Goal, Flags, FlagsStr) :-
+    query_flags(Flags, CombinedFlag),
+    query_flags_str(CombinedFlag, FlagsStr),
+    ffi_call_exc(Goal, CombinedFlag, _, _, _, _).
 
-
-w_atom_ffi(Atom, String) :-
-    with_output_to(string(String), w_atom_ffi_(current_output, Atom)).
-
-atom_ffi(Atom, String) :-
-    with_output_to(string(String), atom_ffi_(current_output, Atom)).
+ffi_call(Goal, Flags, FlagsStr, Exc_0, Exc_qid, Exc_0_2, Rc) :-
+    query_flags(Flags, CombinedFlag),
+    query_flags_str(CombinedFlag, FlagsStr),
+    ffi_call_exc(Goal, CombinedFlag, Exc_0, Exc_qid, Exc_0_2, Rc).
 
 %!  query_flag(?Name, ?Bit)
 %
 %   Flags for  PL_open_query().  Check  with SWI-Prolog.h.   Same code
 %   appears   in  test_ffi.pl.    This  is   duplicated  to   simplify
 %   installation of these tests in the binary version.
+%
+%   This code is mainly for debugging.
 
-% query_flag(debug,		I) => I =0x0001.
-% query_flag(deterministic,	I) => I =0x0100.
-query_flag(normal,		I) => I =0x0002.
-query_flag(nodebug,		I) => I =0x0004.
-query_flag(catch_exception,	I) => I =0x0008.
-query_flag(pass_exception,	I) => I =0x0010.
-query_flag(allow_yield,		I) => I =0x0020.
-query_flag(ext_status,		I) => I =0x0040.
+query_flag(debug,		I) => I =  0x0001.
+query_flag(normal,		I) => I =  0x0002.
+query_flag(nodebug,		I) => I =  0x0004.
+query_flag(catch_exception,	I) => I =  0x0008.
+query_flag(pass_exception,	I) => I =  0x0010.
+query_flag(allow_yield,		I) => I =  0x0020.
+query_flag(ext_status,		I) => I =  0x0040.
+query_flag(deterministic,	I) => I =  0x0100.
+% and pseudo-flags (see XX_Q_* flags in test_ffi.c):
+query_flag(clear_return_true,   I) => I = 0x01000.
+query_flag(close_query,         I) => I = 0x02000.
+
+% TODO: are there any other mutually exclusive flags?
+check_query_flag(Flags) :-
+    query_flag(normal, F1),
+    query_flag(catch_exception, F2),
+    query_flag(pass_exception, F3),
+    Mask is F1 \/ F2 \/ F3,
+    Bits is popcount(Flags /\ Mask),
+    (   Bits =< 1
+    ->  true
+    ;   domain_error(query_flags, Flags)
+    ).
 
 query_flags(Flags, CombinedFlag) :-
     maplist(query_flag, Flags, Ints),
-    aggregate_all(sum(I), member(I, Ints), CombinedFlag).
+    aggregate_all(sum(I), member(I, Ints), CombinedFlag),
+    check_query_flag(CombinedFlag).
+
+ffi_p(a).
+ffi_p(foo(bar)).
+ffi_p(1).
+ffi_p("xyz").
+
+ffi_findall_p(L) :-
+    ffi_findall_(ffi_p(_), L).
+
+ffi_findall_bug_p(L) :-
+    ffi_findall_bug_(ffi_p(_), L).
+
+:- end_tests(call).
